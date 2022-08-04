397e2587c18c4f88a94328bcab30181f8b6d5a87
Emile Mouannes <emile.mouannes@outlook.com>
2022-08-04 18:03:29 -0400

    Add webpack server and boilerplate files

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # odin-Restaurant
+
+This is an assignment for the odin project. The project is a restaurant page
+is dynamically rendered using JS and using node with webpack for module management.
---
+++ dist/main.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+console.log("Emileaaaaa");
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBLDBCIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vb2Rpbi1yZXN0YXVyYW50Ly4vc3JjL2luZGV4LmpzIl0sInNvdXJjZXNDb250ZW50IjpbImNvbnNvbGUubG9nKFwiRW1pbGVhYWFhYVwiKTsiXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
